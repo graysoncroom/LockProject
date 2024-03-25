9b2eb74f3c33d2a4dd49c4ebb31919f12ba1f5f9
Ashton Smith <ajs190019@utdallas.edu>
2024-03-25 00:32:17 -0500

    implementing shared counter variable for demonstration of critical sections in the algorithm and output to match.

--- src/TestSet.h
+++ src/TestSet.h
@@ -1,19 +1,36 @@
 #pragma once
 
 #include <atomic>
+#include <thread>
 
 using namespace std;
 
-// shared lock variable
-atomic<bool> lock(false); 
-
 class TAS {
 private:
+    // our shared lock var
     atomic<bool> lock;
-    //our shared lock 
+    // shared counter for displaying our critical section
+    atomic<int> ctr;
+
+    // our function to increment the counter via threads
+    void incrementCtr(int n) 
+    {
+        //getting the lock to increment the critical section
+        acquire();
+        // incrementing the counter variable and outputting result
+        ctr++;
+        cout << ctr << endl;
+        // releasing the lock for another thread to use
+        release();
+
+        // spinning till every lock is done incrementing
+        while (ctr != n) {
+
+        }
+    }
 
 public:
-	TAS(): lock(false){}
+	TAS(): lock(false), ctr(0) {}
 
     /*
     * Acquire requests the lock which spins
@@ -37,6 +54,17 @@ public:
     {
         // releasing lock
         lock.store(false);
+    }
+
+    void TestAndSet(int n)
+    {
+        
+        // spawning n number of threads to independently function
+        for (int i = 0; i < n; i++)
+        {
+            thread(incrementCtr, n).detach();
+        }
+
     }
 
 };
