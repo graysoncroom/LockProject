a46ff6f11d0f7ced74a9235e67527c61e1987738
William Grayson Croom <grayson.croom@gmail.com>
2024-03-25 21:21:47 -0500

    Fixed impl. bug

--- src/FetchAndIncrement.h
+++ src/FetchAndIncrement.h
@@ -1,39 +1,24 @@
 #pragma once
 
 #include <atomic>
-#include <stdatomic.h>
 #include <thread>
 
-using namespace std;
+class FAILock 
+{
+    std::atomic<int> token_;
+    std::atomic<int> turn_;
+ public:
+    FAILock() : token_(0), turn_(0) {}
 
-class FAILock {
-private:
-	// shared token counter
-	atomic<int> token_;
-	// shared turn counter
-	atomic<int> turn_;
+    void acquire() 
+    {
+        int myturn = std::atomic_fetch_add(&this->token_, 1);
+        while (this->turn_ != myturn);
+    }
 
-
-public:
-	FAILock() : token_(0), turn_(0) {}
-
-	// fetch a value from token for our thread turn 
-	// and increment token ctr before returning initial token val
-	int fetchToken()
-	{
-		// atomically get the value of the token and increment
-		int currToken = atomic_fetch_add(&token_, 1);
-
-		return currToken;
-	}
-
-	// atomically fetch the turn counters value
-	int fetchTurn()
-	{
-		// atomically get the value of our turn counter
-		int currTurn = atomic_load(&turn_);
-
-		return currTurn;
-	}
-
+    void release() 
+    {
+        std::atomic_fetch_add(&this->turn_, 1);
+    }
 };
+
